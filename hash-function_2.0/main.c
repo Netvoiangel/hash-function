f71d89c53c3497ba264b127f984231a5db5b4146
Netvoiangel <110711364+Netvoiangel@users.noreply.github.com>
2022-12-01 12:27:12 +0300

    Initial Commit

---
+++ hash-function_2.0/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  hash-function_2.0
+//
+//  Created by timur on 01.12.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
